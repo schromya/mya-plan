8d2138f2446ccc7d160fc5e38ddfe3ccfe5db36d
schromya <schromya@gmail.com>
2025-01-29 13:07:29 -0600

    Have embeddings working

--- mya-can.py
+++ mya-can.py
@@ -1,28 +1,51 @@
 import os
+from typing import List
 
-import openai
+from openai import OpenAI
 from dotenv import load_dotenv
+import numpy as np
 
 
 
-def query_gpt(prompt:str="", engine:str="text-embedding-ada-002", max_tokens:int=500, temperature:int=0.7, 
+def query_openai(input:List[str]=[""], engine:str="text-embedding-ada-002", max_tokens:int=500, temperature:int=0.7, 
               logprobs:int=1, echo:bool=False):
     
     # Load API key
     load_dotenv()
     openai_api_key = os.getenv('OPENAI_API')
-    openai.api_key = openai_api_key
+  
+    client = OpenAI(api_key=openai_api_key)
 
-    response = openai.Completion.create(engine=engine, 
-                                            prompt=prompt, 
-                                        max_tokens=max_tokens, 
-                                        temperature=temperature,
-                                        logprobs=logprobs,
-                                        echo=echo,
-                                        stop=["\n\n\n"])
+    response = client.embeddings.create(
+        input=input,
+        model=engine
+    )
+
+    return response.data
 
 
-    return response
+def parse_embeddings(embeddings:List[List['OpenAI.embedding']], input:List[List[str]]):
+    """
+    Embedding array should be length of query + number of primitives.
+    """ 
+
+    str_to_embed = {}
+
+    for embed, string in zip(embeddings, input):
+
+        total_embed_score = 0
+        embed_array = embed.embedding
+        for embed_score in embed_array:
+            total_embed_score += embed_score
+        
+        print(string, ":", total_embed_score)
+        str_to_embed[string] = total_embed_score
+    
+    return str_to_embed
+        
+
+
+
 
 
 if __name__ == "__main__":
@@ -51,10 +74,12 @@ if __name__ == "__main__":
     ]
 
     prim_str = " ".join(primitives)
-    prompt = "How would I unscrew a jar a peanut butter using the following primitives: " + prim_str
+    input = ["How would I unscrew a jar a peanut butter?"] + primitives
 
-    print(prompt)
-    response = query_gpt(prompt)
+    
+    response = query_openai(input)
+
+
    
 
     output_file = "openai_response.txt"
@@ -65,3 +90,6 @@ if __name__ == "__main__":
 
     print(f"Response saved to {output_file}")
 
+
+    embed_map = parse_embeddings(response, input)
+    print(embed_map)
