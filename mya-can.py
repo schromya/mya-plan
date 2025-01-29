647ab6eb08025d9246a6b03333d3db4318a98470
schromya <schromya@gmail.com>
2025-01-29 13:24:34 -0600

    Ranked distance from query but seems very random

--- mya-can.py
+++ mya-can.py
@@ -1,4 +1,5 @@
 import os
+import math
 from typing import List
 
 from openai import OpenAI
@@ -29,7 +30,7 @@ def parse_embeddings(embeddings:List[List['OpenAI.embedding']], input:List[List[
     Embedding array should be length of query + number of primitives.
     """ 
 
-    str_to_embed = {}
+    str_to_embed = []
 
     for embed, string in zip(embeddings, input):
 
@@ -39,18 +40,34 @@ def parse_embeddings(embeddings:List[List['OpenAI.embedding']], input:List[List[
             total_embed_score += embed_score
         
         print(string, ":", total_embed_score)
-        str_to_embed[string] = total_embed_score
+        str_to_embed.append([string, total_embed_score])
     
     return str_to_embed
         
 
+def rank_embeddings(embed_map):
+    """
+    TODO change this to array of dictionary
+    """
+    query_embed = embed_map[0][1]
+    primitives = embed_map[1:]
 
+    for prim in primitives:
+        distance = math.fabs(prim[1] - query_embed)
+        prim.append(distance)
+
+    # Sort prim by distance from query embedings
+    primitives = sorted(primitives, key=lambda x: x[2])
+
+    return primitives
+        
 
 
 
 if __name__ == "__main__":
     primitives = [
         "PICK",
+        "END",
         "SCREW",
         "PLACE",
         "WIPE",
@@ -74,22 +91,16 @@ if __name__ == "__main__":
     ]
 
     prim_str = " ".join(primitives)
-    input = ["How would I unscrew a jar a peanut butter?"] + primitives
+    input = ["Open a jar"] + primitives
 
     
     response = query_openai(input)
 
 
-   
-
-    output_file = "openai_response.txt"
-
-    # Write the response to a text file
-    with open(output_file, "w", encoding="utf-8") as file:
-        file.write(str(response))
-
-    print(f"Response saved to {output_file}")
-
-
     embed_map = parse_embeddings(response, input)
     print(embed_map)
+
+    ranked_prims = rank_embeddings(embed_map)
+    print("RANKED PRIMITIVES")
+    for prim in ranked_prims:
+        print(prim[0], prim[1], prim[2])
